5ead701adbc67e542d852d716b921f57c1d21ac5
fiaz7108 <fabiandiaz7108@gmail.com>
2026-03-08 19:36:26 -0500

    up

--- Workshop-3_Exercise_28/Cosole.Main/Program.cs
+++ Workshop-3_Exercise_28/Cosole.Main/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace LaVigaMasResistente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la viga: ");
+            string input = Console.ReadLine();
+
+            Beam beam = new Beam(input);
+            beam.Analyze();
+
+            Console.WriteLine(beam.GetResult());
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
