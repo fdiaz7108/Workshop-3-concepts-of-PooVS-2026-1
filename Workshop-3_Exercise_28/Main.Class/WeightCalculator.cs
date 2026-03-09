e05b8d04b01f065fb7065bd7df457c0e6c222762
fiaz7108 <fabiandiaz7108@gmail.com>
2026-03-08 19:41:10 -0500

    Update

--- Workshop-3_Exercise_28/Main.Class/WeightCalculator.cs
+++ Workshop-3_Exercise_28/Main.Class/WeightCalculator.cs
@@ -1,12 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Main.Class
+namespace LaVigaMasResistente
 {
-    internal class WeightCalculator
+
+    public class WeightCalculator
     {
+
+        public int CalculateTotalWeight(string beamString)
+        {
+
+            int baseResistance = GetBaseResistanceFromChar(beamString[0]);
+
+
+            string restOfBeam = beamString.Substring(1);
+            if (string.IsNullOrEmpty(restOfBeam))
+            {
+                return 0;
+            }
+
+            List<string> tokens = TokenizeBeam(restOfBeam);
+
+
+            return CalculateWeightFromTokens(tokens);
+        }
+
+
+        public int GetBaseResistanceFromChar(char baseChar)
+        {
+            switch (baseChar)
+            {
+                case '%':
+                    return 10;
+                case '&':
+                    return 30;
+                case '#':
+                    return 90;
+                default:
+                    return 0;
+            }
+        }
+
+        private List<string> TokenizeBeam(string beamPart)
+        {
+            List<string> tokens = new List<string>();
+            int i = 0;
+
+            while (i < beamPart.Length)
+            {
+                if (beamPart[i] == '=')
+                {
+                    int count = 0;
+                    while (i < beamPart.Length && beamPart[i] == '=')
+                    {
+                        count++;
+                        i++;
+                    }
+                    tokens.Add(new string('=', count));
+                }
+                else if (beamPart[i] == '*')
+                {
+                    tokens.Add("*");
+                    i++;
+                }
+            }
+
+            return tokens;
+        }
+
+        private int CalculateWeightFromTokens(List<string> tokens)
+        {
+            int totalWeight = 0;
+            int lastBeamWeight = 0;
+
+            for (int j = 0; j < tokens.Count; j++)
+            {
+                string token = tokens[j];
+                if (token[0] == '=')
+                {
+
+                    int n = token.Length;
+                    int segmentWeight = n * (n + 1) / 2;
+                    totalWeight += segmentWeight;
+                    lastBeamWeight = segmentWeight;
+                }
+                else if (token == "*")
+                {
+
+                    if (j < tokens.Count - 1)
+                    {
+                        int connectionWeight = 2 * lastBeamWeight;
+                        totalWeight += connectionWeight;
+                    }
+                }
+            }
+
+            return totalWeight;
+        }
+
+
     }
 }
