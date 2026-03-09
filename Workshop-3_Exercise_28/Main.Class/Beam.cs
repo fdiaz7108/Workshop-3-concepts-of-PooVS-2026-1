ff5d842e374bdd522641503d8e4a4c94f54db51c
fiaz7108 <fabiandiaz7108@gmail.com>
2026-03-08 19:40:28 -0500

    Update

--- Workshop-3_Exercise_28/Main.Class/Beam.cs
+++ Workshop-3_Exercise_28/Main.Class/Beam.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Main.Class;
 
-namespace Main.Class
+namespace LaVigaMasResistente
 {
-    internal class Beam
+    public class Beam
     {
+        private string beamString;
+        private bool isValid;
+        private int resistance;
+        private int weight;
+
+        public Beam(string beamString)
+        {
+            this.beamString = beamString;
+            this.isValid = false;
+            this.resistance = 0;
+            this.weight = 0;
+        }
+
+
+        public void Analyze()
+        {
+            // se valida la viga
+            BeamValidator validator = new BeamValidator();
+            isValid = validator.Validate(beamString);
+
+            if (!isValid) return;
+
+            // se realiza para calcular el peso de la viga 
+            WeightCalculator calculator = new WeightCalculator();
+            weight = calculator.CalculateTotalWeight(beamString);
+
+            // se obtiene la resistencia de la viga 
+            resistance = calculator.GetBaseResistanceFromChar(beamString[0]);
+        }
+
+        public string GetResult()
+        {
+            if (!isValid)
+            {
+                return "La viga está mal construida!";
+            }
+
+            if (weight <= resistance)
+            {
+                return "La viga soporta el peso!";
+            }
+            else
+            {
+                return "La viga NO soporta el peso!";
+            }
+        }
     }
 }
--- Workshop-3_Exercise_28/Main.Class/BeamValidator.cs
+++ Workshop-3_Exercise_28/Main.Class/BeamValidator.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Main.Class
+﻿namespace LaVigaMasResistente
 {
-    internal class BeamValidator
+    public class BeamValidator
     {
+        public bool Validate(string beamString)
+        {
+            if (string.IsNullOrEmpty(beamString))
+            {
+                return false;
+            }
+
+            char firstChar = beamString[0];
+            if (firstChar != '%' && firstChar != '&' && firstChar != '#')
+            {
+                return false;
+            }
+
+            string restOfBeam = beamString.Substring(1);
+
+            if (restOfBeam.Any(c => c != '=' && c != '*'))
+            {
+                return false;
+            }
+
+            if (restOfBeam.Length > 0 && restOfBeam[0] != '=')
+            {
+                return false;
+            }
+
+            if (restOfBeam.Contains("**"))
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
